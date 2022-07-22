7c64571833b4b23e5c0e19d74f02bb74f27b0d34
Justin Blanchard <justinhblanchard@gmail.com>
2022-07-22 15:34:48 -0400

    finished parse

--- parse.py
+++ parse.py
@@ -1,7 +1,13 @@
 import sys
 import pandas as pd
-filename = sys.argv[1]
-df = pd.read_csv('/Users/ihz/Desktop/Covid_Research/data/' + filename)
-df = df.dropna()
+df = pd.read_csv(sys.argv[1])
 df = df.drop_duplicates()
-df.to_csv('/Users/ihz/Desktop/Covid_Research/data/parsed.csv', index = False)
+df1 = df.select_dtypes(include = 'object')
+for i in df1.columns:
+    if i != 'dates' and i != 'Dates' and i != 'date' and i != 'Date' and i != 'state' and i != 'State' and i != 'states' and i != 'States' and i != 'country' and i != 'Country' and i != 'countries' and i != 'Countries':
+        df1 = df1.drop(i, axis = 1)
+df = df.select_dtypes(exclude = 'object')
+df = pd.concat([df1, df], axis = 1)
+df.to_csv('parsed.csv', index = False)
+for i in df.iloc[:, 1].unique():
+    df.loc[(df.iloc[:, 1] == i)].to_csv(i + '.csv', index = False)
