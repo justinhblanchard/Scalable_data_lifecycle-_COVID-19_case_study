0e524f82e8c25b3f38e98554c9d561dfeffdd6f4
justinhblanchard <107429100+justinhblanchard@users.noreply.github.com>
2022-07-25 15:28:40 -0400

    Update pca_graph.py

--- pca_graph.py
+++ pca_graph.py
@@ -1,11 +1,13 @@
+import sys
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 import numpy as np
 from sklearn.decomposition import PCA
-df = pd.read_csv('/Users/ihz/Desktop/Covid_Research/data/parsed')
+df = pd.read_csv('/Users/ihz/Desktop/covid/standardized.csv')
 df1 = df.select_dtypes(include = 'object')
 df = df.select_dtypes(exclude = 'object')
+df = df.dropna()
 pca = PCA(n_components = 2)
 df = pca.fit_transform(df)
 df = pd.DataFrame(df)
@@ -14,11 +16,11 @@ df.reset_index(drop = True, inplace = True)
 df1.reset_index(drop = True, inplace = True)
 df1 = pd.concat([df1, df], axis = 1)
 vals = np.arange(pca.n_components_) + 1
-df1.to_csv('/Users/ihz/Desktop/Covid_Research/data/pca.csv', index = False)
+df1.to_csv('/Users/ihz/Desktop/covid/pca.csv', index = False)
 sns.scatterplot(data = df1, x = pca.explained_variance_ratio_[0], y = pca.explained_variance_ratio_[1], hue = 'location', alpha = 0.3)
 c = np.transpose(pca.components_[0:2, :])
 for i in range(c.shape[0]):
     plt.arrow(0, 0, c[i, 0] * 3, c[i, 1] * 3)
     plt.text(c[i,0] * 3.1, c[i,1] * 3.1, i + 1, alpha = 0.5)
 plt.grid()
-plt.savefig('/Users/ihz/Desktop/Covid_Research/plots/pca.png', bbox_inches = 'tight')
+plt.savefig('/Users/ihz/Desktop/covid/pca.png', bbox_inches = 'tight')
