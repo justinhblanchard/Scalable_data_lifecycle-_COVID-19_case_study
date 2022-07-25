be39ad9e72ddf895416a3621f5dcb1478bdc4c74
justinhblanchard <107429100+justinhblanchard@users.noreply.github.com>
2022-07-25 15:28:21 -0400

    Update standardize.py

--- standardize.py
+++ standardize.py
@@ -1,9 +1,10 @@
+import sys
 import pandas as pd
 from sklearn.preprocessing import StandardScaler
-df = pd.read_csv('/Users/ihz/Desktop/Covid_Research/data/parsed.csv')
+df = pd.read_csv('/Users/ihz/Desktop/covid/' + sys.argv[1])
 scale = StandardScaler()
 df_scaled = scale.fit_transform(df.select_dtypes(exclude = 'object'))
 df1 = df.select_dtypes(include = 'object')
 df_scaled = pd.DataFrame(df_scaled, columns = df.select_dtypes(exclude = 'object').columns)
 df1 = pd.concat([df1, df_scaled], axis = 1)
-df1.to_csv('/Users/ihz/Desktop/Covid_Research/data/standardized', index = False)
+df1.to_csv('/Users/ihz/Desktop/covid/' + sys.argv[1], index = False)
